12aca558d896b2d934e34cc3bee095f8ac8d40f1
kbs <clashrock82@naver.com>
2022-02-26 15:18:56 +0900

    기초문법설명

--- daily01/src/chapter02/HelloWorld.java
+++ daily01/src/chapter02/HelloWorld.java
@@ -1,5 +1,25 @@
-package chapter02;
 
+// 1. 주석문
+// 컴파일러는 해석을 안하고 넘어가는 문장들
+//사용목적은 : 개발자(사림)이 읽고 참고하도록 써넣은 정보.
+
+// a) 한줄주석
+//  <--한줄주석
+// b) 여러줄 주석
+/*
+   여러줄 주석입니다. 
+   냐옹냐옹
+ */
+
+
+//Helloworld 클래스
+package chapter02; 
+
+     // Helloworld 프로그램의 시작포인트
 public class HelloWorld {
 
+  public static void main(String[] args)  {
+     System.out.println("하악하악, Helloworld");
+  }
+  
 }
